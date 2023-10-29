2d0b8f6dfe14f2055049ec8aa03acbf595d56ae7
Frances Morales Velilla <frances0688@gmail.com>
2023-10-29 15:25:03 +0100

    Exercise 4 - create endpoint to show category by id

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -31,6 +31,15 @@ const CategoryController = {
                 throw err;
             res.send(result);
         });
+    },
+
+    getById(req, res) {
+        let sql = `SELECT * FROM categories WHERE id = ${req.params.id}`;
+        db.query(sql, (err, result) => {
+            if (err) 
+                throw err;
+            res.send(result);
+        });
     }
 };
 
--- routes/categories.js
+++ routes/categories.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const CategoryController = require("../controllers/CategoryController.js");
 
 router.get("/", CategoryController.getAll);
+router.get("/getById/:id", CategoryController.getById);
 router.post("/create", CategoryController.create);
 router.put("/update/:id", CategoryController.update);
 
